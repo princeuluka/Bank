85d704a3691787f6625188d0c5222fde0ad044cb
Prince Uluka <ulukaprince1996@gmail.com>
2022-09-01 07:17:32 +0100

    Updates

--- Bank/DataAccess/Data/ApplicationDbContext.cs
+++ Bank/DataAccess/Data/ApplicationDbContext.cs
@@ -2,7 +2,6 @@
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
-using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,13 +21,7 @@ namespace DataAccess.Data
         public DbSet<LGA> LGAs { get; set; }
         public DbSet<State> States { get; set; }
 
-        protected override void OnModelCreating(DbModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<State>()
-                .HasRequired<CustomerData>(s => s.Name)
-                .WithMany(g => g.StateID)
-                .HasForeignKey<int>(s => s.StateID);
-        }
+        
     }
 
    
