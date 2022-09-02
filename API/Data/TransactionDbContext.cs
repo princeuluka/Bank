b352df53f89891535dfa49685c1c60084a91d620
Prince Uluka <ulukaprince1996@gmail.com>
2022-09-02 09:17:35 +0100

    Updates

---
+++ API/Data/TransactionDbContext.cs
@@ -0,0 +1,19 @@
+﻿using DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class TransactionDbContext : DbContext
+    {
+        public TransactionDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<CustomerData> Customers { get; set; }
+        public DbSet<LGA> LGAs { get; set; }
+        public DbSet<State> States { get; set; }
+        public DbSet<CustomerAccount> Accounts { get; set; }
+        public DbSet<Transactions> Transactions { get; set; }
+
+    }
+}
